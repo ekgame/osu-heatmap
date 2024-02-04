23517d52e206361a8dc801cc4e1a9c6c6abdb328
ekgame <ekgame1@gmail.com>
2024-02-04 10:07:03 +0200

    Moved button styles to button element.

--- components/BasicButton.vue
+++ components/BasicButton.vue
@@ -3,3 +3,17 @@
         <slot></slot>
     </button>
 </template>
+
+<style scoped>
+.btn {
+    padding: 0.5em 1em;
+    background-color: var(--button-color);
+    border-radius: var(--button-radius);
+    color: var(--button-text-color);
+}
+
+.input-group > .btn:last-child {
+    border-top-left-radius: 0;
+    border-bottom-left-radius: 0;
+}
+</style>
